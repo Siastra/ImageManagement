5bc49b869570045e1196a597b5aab9448266ad54
Sebastian2123 <sebastian_schramm@hotmail.com>
2021-01-03 13:27:18 +0100

    Create Post

--- inc/createPost.php
+++ inc/createPost.php
@@ -1,35 +1,44 @@
 
-
-<html>
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-</head>
-<body>
-
-<form method="post" action="createPost.php" enctype="multipart/form-data">
+<form method="post" action="index.php?section=create&upload=true" enctype="multipart/form-data">
 	<input type="file" name="picture">
     </br>
-    <input type="text" placeholder="Tags " name="tags">
+    <input type="text" placeholder="Tags " name="tags" id="tags">
     </br>
 	<input type="submit" name="upload" value="Upload" />
 
 </form>
 <?php
-include_once "../utility/DB.php";
-include_once "../model/User.php";
-
-
 $db = new DB();
-if(isset($_FILES["picture"])){
-    $z=$_FILES["picture"]["name"];
-    $pathway="D:\\Pathway\\to\\img\\".$z.".jpg";
-    move_uploaded_file($_FILES["picture"]["tmp_name"],$pathway);
-    $path=$pathway;
-    $restricted = 0;
-    $db->createPost($path,$restricted);
+include_once "utility/DB.php";
+include_once "model/User.php";
+$z=$_SESSION["username"];
+$spath = "pictures\\full\\$z\\";
+
+if(!is_dir($spath)){
+    mkdir($spath,0700);
+}
+if(isset($_GET["upload"])){
+   
+    $name=$_FILES["picture"]["name"];
+    $pathway="pictures\\full\\$z\\$name";
+        move_uploaded_file($_FILES["picture"]["tmp_name"],$pathway );
+        $restricted = 0;
+        if(isset($_POST["tags"])){
+            $tag=$_POST["tags"];
+                  $db->checkTag($tag);
+                $result=$db->createPost($pathway,$restricted);
+                $db->setTag($result,$tag);
+
+            
+        }
+
+    }
+    $post = $db->showDashboardself();
+
+    $post2 = array_reverse($post);
+
+    foreach($post2 as $dash) {
+        echo "<img class=pic src=" .$dash["path"]."   />";
     }
 
 ?>
-</body>
-</html>
